4bd64b2497932a0f3675588b2dae0c734100fd51
The Wizard <thewizard@gwendolyn.life>
2023-06-16 23:08:07 -0700

    entities/__init__.py -> adds ChatResponse import

--- entities/__init__.py
+++ entities/__init__.py
@@ -1,5 +1,6 @@
 """ Top level importable package for all entities """
 from .assistant import Assistant
 from .chat import Chat
+from .chat_response import ChatResponse
 from .system import System
 from .user import User
