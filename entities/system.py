b7aebf06e4e22a63e9476761ad82d36c218063b0
The Wizard <thewizard@gwendolyn.life>
2023-06-20 13:52:39 -0700

    system.py -> removes `message` param requirement
    Changes double quotes to singles

--- entities/system.py
+++ entities/system.py
@@ -4,6 +4,7 @@ from .role import Role
 
 class System(Role):
     """ System entity """
-    def __init__(self, message: str, name: str = ""):
-        super().__init__(message, name)
-        self.role = "system"
+    def __init__(self, message: str, name: str = ''):
+        super().__init__(name)
+        self.content = message
+        self.role = 'system'
